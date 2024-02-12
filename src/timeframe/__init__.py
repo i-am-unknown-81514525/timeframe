89a8ba2bfbd6ef196f319d7737f1de6672fec688
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-02-12 21:52:53 +0000

    Switch to `poetry` build tool

--- src/timeframe/__init__.py
+++
@@ -1,8 +0,0 @@
-from .timeframe import (TimeFrame,
-                        Event,
-                        Action,
-                        Attempt,
-                        IterationFailed,
-                        IterationCompleted,
-                        Emoji,
-                        State)
