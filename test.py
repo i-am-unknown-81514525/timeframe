14fc3b5bbd4af2d33bbce7f6f2d5340ccb800e44
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2024-04-23 17:26:45 +0100

    Changed text file for the change

--- test.py
+++ test.py
@@ -17,27 +17,27 @@ async def test() -> None:
     import time
 
     with TimeFrame('test args', name='Text request', rt=func, test_kwargs='test kwargs') as time_frame:
-        with time_frame.create(name='Prompt') as group_prompt:
-            with group_prompt.create(name='Prompt request', retries=5, ignore_retries=(TimeoutError,), check_exc_subclass=True) as event_frame:
-                function_call = False
+        with time_frame.create_action(name='Prompt request', retries=5, ignore_retries=(TimeoutError,),
+                                 check_exc_subclass=True) as event_frame:
+            function_call = False
+            for frame in event_frame:
+                async with frame:
+                    time.sleep(random.uniform(0, 0.4))  # Stimulate doing network operation
+                    if random.random() < 0.2:
+                        raise ValueError  # Stimulate a complete random chance of a network error occur
+                    if random.random() < 0.3:
+                        raise TimeoutError  # Stimulate a complete internet breakdown
+                    if random.random() < 0.9:
+                        function_call = True
+        if function_call:
+            with time_frame.create_action(name='Function call `mul`', retries=0):
+                time.sleep(random.uniform(0, 0.01))  # Stimulate doing network operation
+            with time_frame.create_action(name='Function Response', retries=5) as event_frame:
                 for frame in event_frame:
                     async with frame:
                         time.sleep(random.uniform(0, 0.4))  # Stimulate doing network operation
-                        if random.random() < 0.2:
+                        if random.random() < 0.7:
                             raise ValueError  # Stimulate a complete random chance of a network error occur
-                        if random.random() < 0.3:
-                            raise TimeoutError # Stimulate a complete internet breakdown
-                        if random.random() < 0.9:
-                            function_call = True
-            if function_call:
-                with group_prompt.create(name='Function call `mul`', retries=0):
-                    time.sleep(random.uniform(0, 0.01))  # Stimulate doing network operation
-                with group_prompt.create(name='Function Response', retries=5) as event_frame:
-                    for frame in event_frame:
-                        async with frame:
-                            time.sleep(random.uniform(0, 0.4))  # Stimulate doing network operation
-                            if random.random() < 0.7:
-                                raise ValueError  # Stimulate a complete random chance of a network error occur
     print(time_frame.traceback_format())
     print('---Display on discord---')
     print(time_frame.frame_format_dc())
